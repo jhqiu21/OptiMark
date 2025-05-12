47ce5c80a4f58b89114ae54514dd0ee67378710d
jhqiu21 <142405039+jhqiu21@users.noreply.github.com>
2025-05-12 11:32:57 +0800

    Update clean.sql

--- db/clean.sql
+++ db/clean.sql
@@ -1,10 +1,12 @@
-
-DROP TABLE IF EXISTS users;
+-- drop table
+DROP TABLE IF EXISTS students;
+DROP TABLE IF EXISTS staff;
 DROP TABLE IF EXISTS semesters;
 DROP TABLE IF EXISTS courses;
-DROP TABLE IF EXISTS course_offerings;
-DROP TABLE IF EXISTS offering_teachers;
+DROP TABLE IF EXISTS offers;
+DROP TABLE IF EXISTS offering_instructor;
 DROP TABLE IF EXISTS offering_students;
 DROP TABLE IF EXISTS exams;
 DROP TABLE IF EXISTS upload_batches;
 DROP TABLE IF EXISTS answer_tasks;
+DROP TABLE IF EXISTS exam_files;
